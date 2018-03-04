31a6a1d0797faeb334203270ee9b97f05012d4ad
moreno <moro2694@gmail.com>
2018-03-04 23:08:32 +0100

    added follower classification features notebook

--- followers_classification/followers_classifier.py
+++ followers_classification/followers_classifier.py
@@ -4,36 +4,37 @@ from sklearn.model_selection import cross_val_predict
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
 from prepare_dataset import prep_follower_dataset
 from plotting import plot_confusion_matrix
 
 
 folds = 4
 train_xs, train_ys, test_xs, test_ys = prep_follower_dataset(
-    account_features=['followers_count', 'following_count'],
-    post_features= ['posts_count', 'mean_likes', 'mean_comments'],
-    features_to_scale=['followers_count', 'following_count'] + ['posts_count', 'mean_likes', 'mean_comments'],
-    new_one=True,
+    account_features=['num_posts', 'cons_bio_sim', 'oth_bio_sim', 'followers_count', 'following_count'],
+    post_features=['posts_count'],# 'mean_likes', 'mean_comments'],
+    features_to_scale=['num_posts', 'cons_bio_sim', 'oth_bio_sim', 'posts_count', 'followers_count', 'following_count'],
+    new_one=False,
     folds=None)
 
 models = [
     LogisticRegression(random_state=0, solver='liblinear'),
     SVC(probability=True),
-    KNeighborsClassifier(5)
+    KNeighborsClassifier(5),
+    RandomForestClassifier()
 ]
 
-scoring = ['average_precision', 'roc_auc', 'f1_micro']
+scoring = ['average_precision', 'roc_auc', 'f1_micro', 'f1_macro']
 
 for model in models:
     print('\n' + type(model).__name__ + '\n')
     scores = cross_validate(model, train_xs, train_ys, cv=4, scoring=scoring, return_train_score=False)
     predicted = cross_val_predict(model, train_xs, train_ys, cv=4)
-
     for score in scoring:
         print(score + ' mean: ' + str(scores['test_' + score].mean()))
-
     cnf_matrix = confusion_matrix(train_ys, predicted)
     plt.figure()
-    plot_confusion_matrix(cnf_matrix, ['Other', 'Consumer'], title=type(model).__name__)
+    plot_confusion_matrix(cnf_matrix, ['Consumer', 'Other'], title=type(model).__name__)
     plt.show()
--- followers_classification/prepare_dataset.py
+++ followers_classification/prepare_dataset.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import re
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import KFold
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def prep_follower_dataset(account_features=[], post_features=[], features_to_scale=[], new_one=False, folds=None):
@@ -27,9 +29,9 @@ def prep_follower_dataset(account_features=[], post_features=[], features_to_sca
         print('generating new dataset')
         result = get_labels()
         if account_features:
-            result = pd.merge(result, get_follower_accounts(account_features), on='username')
+            result = pd.merge(result, get_follower_accounts(account_features, result['username'].to_frame()), on='username')
         if post_features:
-            result = pd.merge(result, get_follower_posts(post_features),  on='username')
+            result = pd.merge(result, get_follower_posts(post_features, result['username'].to_frame()),  on='username')
         if features_to_scale:
             result[features_to_scale] = StandardScaler().fit_transform(X=result[features_to_scale])
         if not folds:
@@ -61,32 +63,54 @@ def get_labels():
     labels_indices = []
     for label in labels_data.iloc[:, 1:].as_matrix():
         if np.where(label > 0)[0][0] > 0:
-            labels_indices.append(0)
-        else:
             labels_indices.append(1)
+        else:
+            labels_indices.append(0)
     labels_df = pd.DataFrame(data=labels_indices, columns=['label'], dtype=np.int8)
     labels_data = pd.concat([labels_data['username'], labels_df], axis=1)
     return labels_data
 
 
-def get_follower_accounts(features):
+def get_follower_accounts(features, users=None):
     names = ['user_id', 'username', 'profile_pic', 'followers_count', 'following_count',
              'num_posts', 'bio', 'isPrivate']
-    cols = features and names
-    # username column needed to join data with labels
+    stop_words = ['and', 'or', 'before', 'a', 'an', 'the', 'bio', 'is', 'all', 'to', 'for', 'by', 'in', 'of', 'we',
+                  'our', 'at', 'my', 'be']
     features.insert(0, 'username')
-    followers_data = pd.read_csv('data/users.csv', delimiter=',', names=names, usecols=cols)
-    # converting isPrivate from string to boolean
-    if 'isPrivate' in cols:
-        followers_data['isPrivate'] = followers_data['isPrivate'].map({'True': 1, 'False': 0})
-    # removing nan values
+    followers_data = pd.read_csv('data/users.csv', delimiter=',', names=names)
+
+    # cleaning
+    followers_data['bio'] = followers_data['bio'].fillna('bio')
+    followers_data['bio'] = followers_data['bio'].apply(clean_bios)
     followers_data[['followers_count', 'following_count', 'num_posts']] = followers_data[
         ['followers_count', 'following_count', 'num_posts']].apply(pd.to_numeric, errors='coerce')
     followers_data = followers_data.fillna(0)
-    return followers_data
+    followers_data['isPrivate'] = followers_data['isPrivate'].map({'True': 1, 'False': 0})
+
+    # selecting requested users
+    if users is not None:
+        followers_data = users.merge(followers_data, on='username')
+
+    # adding features
+    if 'cons_bio_sim' in features:
+        cons_vectorizer = CountVectorizer(min_df=3, stop_words=stop_words,
+                                          vocabulary=['fashion', 'com', 'life', 'designer', 'director', 'love',
+                                                      'london', 'me', 'creative', 'founder', 'gmail', 'with', 'lover',
+                                                      'stylist', 'travel'])
+
+        cons_x = cons_vectorizer.fit_transform(followers_data['bio'])
+        followers_data['cons_bio_sim'] = pd.Series(np.squeeze(np.asarray(cons_x.sum(axis=1))))
+    if 'oth_bio_sim' in features:
+        others_vectorizer = CountVectorizer(min_df=3, stop_words=stop_words,
+                                            vocabulary=['com', 'fashion', 'shop', 'new', 'de', 'gmail', 'info', 'with',
+                                                        'brand', 'shipping', 'jewelry', 'worldwide', 'online', 'us',
+                                                        'world'])
+        oth_x = others_vectorizer.fit_transform(followers_data['bio'])
+        followers_data['oth_bio_sim'] = pd.Series(np.squeeze(np.asarray(oth_x.sum(axis=1))))
+    return followers_data[features]
 
 
-def get_follower_posts(features):
+def get_follower_posts(features, users=None):
     brands = ['athenaprocopiou', 'daftcollectionofficial', 'dodobaror', 'emporiosirenuse', 'heidikleinswim',
               'lisamariefernandez', 'loupcharmant', 'miguelinagambaccini', 'muzungusisters', 'zeusndione']
 
@@ -100,6 +124,9 @@ def get_follower_posts(features):
     # posts data cleaning
     posts_data.drop_duplicates(subset='id_post', inplace=True)
     posts_data[['likes_count', 'comment_count']] = posts_data[['likes_count', 'comment_count']].apply(pd.to_numeric)
+
+    if users is not None:
+        posts_data = users.merge(posts_data, on='username')
 
     # aggregating posts data to get user features
     posts_count = posts_data.groupby(['username']).count()['id_post'].to_frame().reset_index()
@@ -125,3 +152,12 @@ def write_dataset_csv(result, features=None, i=None, train_index=None, test_inde
         result.iloc[train_index]['label'].to_csv('data/test/train_ys_' + str(i) + '.csv', index=False, header=True)
         result.iloc[test_index]['label'].to_csv('data/test/test_ys_' + str(i) + '.csv', index=False, header=True)
     print('dataset saved to csv')
+
+
+def clean_bios(bio):
+    bio = bio.replace('b\'', '')
+    bio = bio.replace('\\n', '')
+    bio = bio.replace('\\r', '')
+    bio = bio.replace('\\\\x', '<')
+    bio = re.sub('<\w\w', '', bio)
+    return bio
