9dbd120d66addd6fed8dac4f1daec7fdc16a476b
moreno <moro2694@gmail.com>
2018-04-15 10:05:57 +0200

    added viz for Francesca

--- web_server/leSirenuse/static/js/single.js
+++ web_server/leSirenuse/static/js/single.js
@@ -1,13 +1,181 @@
-$(document).ready(fucntion(){
-  var GET = {};
-  var query = window.location.search.substring(1).split("&");
-  for (var i = 0, max = query.length; i < max; i++)
-  {
-      if (query[i] === "") // check for trailing & with no param
-          continue;
+d3.json("http://localhost:8000/es_tool/json/brands_sim.json").then(function(result){
+	data = result['brands_sim'];
+	var svg = d3.select("#scatter");
+	width = parseInt(window.getComputedStyle(document.getElementById("scatter-brands")).getPropertyValue("width").slice(0,-2));
+	height =  parseInt(window.getComputedStyle(document.getElementById("scatter-brands")).getPropertyValue("height").slice(0,-2));
+	svg.attr("viewBox", "-50 -50 "+width+" "+height);
+	var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+		y = d3.scaleLinear().rangeRound([height, 0]);
 
-      var param = query[i].split("=");
-      GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
-  }
-  ("#post").attr("src", GET['post']);
-})
+	var g = svg.append("g");
+	
+	x.domain(data.map(function(d) { return d.name; }));
+	y.domain([0, d3.max(data, function(d) { return d.sim; })]);
+
+	g.append("g")
+	  .attr("class", "axis axis--x")
+	  .attr("transform", "translate(0," + height + ")")
+	  .call(d3.axisBottom(x));
+
+	g.append("g")
+	  .attr("class", "axis axis--y")
+	  .call(d3.axisLeft(y).ticks(10, "%"))
+	.append("text")
+	  .attr("transform", "rotate(-90)")
+	  .attr("y", 6)
+	  .attr("dy", "0.71em")
+	  .attr("text-anchor", "end")
+	  .text("Brand");
+
+	g.selectAll(".bar")
+	.data(data)
+	.enter().append("circle")
+	  .attr("class", "bar")
+	  .attr("cx", function(d) { return x(d.name); })
+	  .attr("cy", function(d) { return y(d.sim); })
+	  .attr("width", x.bandwidth())
+	  //.attr("height", function(d) { return height - y(d.frequency); });
+	  .attr("r", "5");
+});
+
+
+d3.json("http://localhost:8000/es_tool/json/caption.json").then(function(dataset){
+	width = parseInt(window.getComputedStyle(document.getElementById("caption")).getPropertyValue("width").slice(0,-2));
+	height =  parseInt(window.getComputedStyle(document.getElementById("caption")).getPropertyValue("height").slice(0,-2));
+	
+	var colors = []
+    for(i=0; i<dataset.children.length; i++){
+      colors.push("#"+((1<<24)*Math.random()|0).toString(16));
+    };
+	var bubble = d3.pack(dataset)
+		.size([height, width])
+		.padding(1.05);
+
+	var svg = d3.select("#caption")
+		.append("svg");
+	svg.attr("viewBox", "65 0 "+width+" "+height);
+
+	var nodes = d3.hierarchy(dataset)
+		.sum(function(d) { return d.score; });
+
+	var node = svg.selectAll(".node")
+		.data(bubble(nodes).descendants())
+		.enter()
+		.filter(function(d){
+			return  !d.children
+		})
+		.append("g")
+		.attr("class", "node")
+		.attr("transform", function(d) {
+			return "translate(" + d.x + "," + d.y + ")";
+		});
+
+	node.append("title")
+		.text(function(d) {
+			return d.name + ": " + d.score;
+		});
+
+	node.append("circle")
+		.attr("r", function(d) {
+			return d.r;
+		})
+		.attr("fill", function(d,i){ return colors[i];});
+	
+	node.append("text")
+		.attr("dy", ".2em")
+		.style("text-anchor", "middle")
+		.text(function(d) {
+			console.log(d);
+			return d.data.topic;
+		})
+		.attr("font-family", "sans-serif")
+		.attr("font-size", function(d){
+			return d.r/4;
+		})
+		.attr("fill", "white");
+
+	node.append("text")
+		.attr("dy", "1.3em")
+		.style("text-anchor", "middle")
+		.text(function(d) {
+			console.log(d);
+			return d.data.score;
+		})
+		.attr("font-family",  "Gill Sans", "Gill Sans MT")
+		.attr("font-size", function(d){
+			return d.r/3;
+		})
+		.attr("fill", "white");
+
+});
+
+
+d3.json("http://localhost:8000/es_tool/json/tags.json").then(function(dataset){
+	width = parseInt(window.getComputedStyle(document.getElementById("caption")).getPropertyValue("width").slice(0,-2));
+	height =  parseInt(window.getComputedStyle(document.getElementById("caption")).getPropertyValue("height").slice(0,-2));
+	
+	var colors = []
+    for(i=0; i<dataset.children.length; i++){
+      colors.push("#"+((1<<24)*Math.random()|0).toString(16));
+    };
+	var bubble = d3.pack(dataset)
+		.size([height, width])
+		.padding(1.05);
+
+	var svg = d3.select("#caption")
+		.append("svg");
+	svg.attr("viewBox", "65 0 "+width+" "+height);
+
+	var nodes = d3.hierarchy(dataset)
+		.sum(function(d) { return d.score; });
+
+	var node = svg.selectAll(".node")
+		.data(bubble(nodes).descendants())
+		.enter()
+		.filter(function(d){
+			return  !d.children
+		})
+		.append("g")
+		.attr("class", "node")
+		.attr("transform", function(d) {
+			return "translate(" + d.x + "," + d.y + ")";
+		});
+
+	node.append("title")
+		.text(function(d) {
+			return d.name + ": " + d.score;
+		});
+
+	node.append("circle")
+		.attr("r", function(d) {
+			return d.r;
+		})
+		.attr("fill", function(d,i){ return colors[i];});
+	
+	node.append("text")
+		.attr("dy", ".2em")
+		.style("text-anchor", "middle")
+		.text(function(d) {
+			console.log(d);
+			return d.data.topic;
+		})
+		.attr("font-family", "sans-serif")
+		.attr("font-size", function(d){
+			return d.r/4;
+		})
+		.attr("fill", "white");
+
+	node.append("text")
+		.attr("dy", "1.3em")
+		.style("text-anchor", "middle")
+		.text(function(d) {
+			console.log(d);
+			return d.data.score;
+		})
+		.attr("font-family",  "Gill Sans", "Gill Sans MT")
+		.attr("font-size", function(d){
+			return d.r/3;
+		})
+		.attr("fill", "white");
+
+});
