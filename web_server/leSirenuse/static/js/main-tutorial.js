22cb693c6aaba7a2fb6ebddc9f51025cf09984c6
Francesca <francesca1morini@gmail.com>
2018-05-01 15:10:17 +0200

    tutorial fixes

--- web_server/leSirenuse/static/js/main-tutorial.js
+++ web_server/leSirenuse/static/js/main-tutorial.js
@@ -10,3 +10,68 @@ $('.js-anchor-link').click(function(e){
     $('body, html').animate({scrollTop: scrollTo+'px'}, 300);
   }
 });
+
+/*loader logo*/
+
+var sotto = document.getElementById("sotto");
+	var style = window.getComputedStyle(sotto);
+	//d3.select("#sotto").style("margin-left", style.marginLeft)
+
+  var scompari = function(){
+  d3.select("#sotto").style("margin-left", style.marginLeft)
+  d3.select("#sotto").style("margin-right", "auto")
+	d3.select("#sopra")
+	.transition()
+	.duration(1000)
+	.style("color", "#4d7a89")
+
+	d3.select("#sotto")
+	.transition()
+	.duration(1000)
+	.style("width", "0px")
+	setTimeout(function() { appari() }, 1000)
+}
+
+
+var appari = function(){
+	d3.select("#sopra")
+	.transition()
+	.duration(1000)
+	.style("color", "white")
+
+	d3.select("#sotto")
+	.transition()
+	.duration(1000)
+	.style("width", "110px")
+	setTimeout(function() { scomparidestra() }, 1000)
+}
+/*secondo ciclo*/
+var scomparidestra = function(){
+d3.select("#sotto").style("margin-right", style.marginRight)
+d3.select("#sotto").style("margin-left", "auto")
+d3.select("#sopra")
+.transition()
+.duration(1000)
+.style("color", "#4d7a89")
+
+d3.select("#sotto")
+.transition()
+.duration(1000)
+.style("width", "0px")
+setTimeout(function() { apparidestra() }, 1000)
+}
+
+
+var apparidestra = function(){
+d3.select("#sopra")
+.transition()
+.duration(1000)
+.style("color", "white")
+
+d3.select("#sotto")
+.transition()
+.duration(1000)
+.style("width", "110px")
+setTimeout(function() { scompari() }, 1000)
+}
+scompari();
